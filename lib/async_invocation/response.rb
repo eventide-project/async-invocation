e64bfa9dde74209b02917f322492cc64ddaf12ee
Scott Bellware <sbellware@gmail.com>
2015-07-13 22:16:31 -0500

    Applicative errors derive from RuntimeError

--- lib/async_invocation/response.rb
+++ lib/async_invocation/response.rb
@@ -1,6 +1,6 @@
 module AsyncInvocation
   class Response
-    class Error < StandardError; end
+    class Error < RuntimeError; end
 
     def self.method_missing(meth, *args)
       raise Error, "Async operation `#{meth}' does not return results. It must be called with a block argument (or, \"callback\")."
