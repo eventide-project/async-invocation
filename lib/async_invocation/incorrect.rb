62aef8dd6f27721b380ab42fe4ec538f1e8bf79d
Scott Bellware <sbellware@gmail.com>
2016-06-05 12:52:24 -0700

    Error message is clarified. Mention of 'callback' is misleading and is removed.

--- lib/async_invocation/incorrect.rb
+++ lib/async_invocation/incorrect.rb
@@ -3,7 +3,7 @@ module AsyncInvocation
     class Error < RuntimeError; end
 
     def self.method_missing(meth, *args)
-      raise Error, "Incorrect invocation of async operation `#{meth}'. It does not return results. It must be called with a block argument that will be used as a callback."
+      raise Error, "Incorrect invocation of async operation `#{meth}'. It does not return results. It must be called with a block argument."
     end
   end
 end
