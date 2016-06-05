58b5e5ed87896c100c05a60a280148a0be2e392f
Scott Bellware <sbellware@gmail.com>
2016-06-05 02:16:13 -0700

    Vestigial code is removed

--- test/spec/incorrect.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'spec_init'
-
-describe "AsyncInvocation Response" do
-  specify "Raises an error when used synchronously" do
-    assert_raises AsyncInvocation::Incorrect::Error do
-      AsyncInvocation::Incorrect.something
-    end
-  end
-end
