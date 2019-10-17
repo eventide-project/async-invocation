e3c016a134cf0d57eb651d54fb7e2babae283961
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-17 16:29:22 -0500

    Parenthesis are added to assert_raises and refute_raises

--- test/automated/incorrect.rb
+++ test/automated/incorrect.rb
@@ -2,7 +2,7 @@ require_relative './automated_init'
 
 context "AsyncInvocation Response" do
   test "Raises an error when used synchronously" do
-    assert_raises AsyncInvocation::Incorrect::Error do
+    assert_raises(AsyncInvocation::Incorrect::Error) do
       AsyncInvocation::Incorrect.something
     end
   end
